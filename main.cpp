955f911518067b6542c55faded8cff076a3befce
shixinsb <shixinsb@sina.com>
2017-04-01 10:30:21 +0200

    Texture class

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 // GLFW
 #include <GLFW/glfw3.h>
 #include "Shader.h"
+#include "Texture.h"
+#include <SOIL.h>
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 void key_callback(GLFWwindow* window, int key, int scancode, int action,
@@ -44,30 +46,32 @@ int main() {
 	}
 
 	/* Define the viewport dimensions */
-	int width, height;
+	int width[2], height[2];
 	// Automatically get the buffer size
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+	//glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, WIDTH, HEIGHT);
 
 	/* key function */
 	glfwSetKeyCallback(window, key_callback);
 
 	Shader ourShader("shader/default.vs", "shader/default.frag");
 
-	/* first triangle */
-	GLfloat vertices[] = { 0.5f, 0.5f, 0.0f, // Top Right
-			0.5f, -0.5f, 0.0f, // Bottom Right
-			-0.5f, -0.5f, 0.0f // Bottom Left
-			//-0.5f, 0.5f, 0.0f // Top Left
+	/* texture */
+	glActiveTexture(GL_TEXTURE0);
+	Texture texture1("texture/container.jpg");
+	glActiveTexture(GL_TEXTURE1);
+	Texture texture2("texture/awesomeface.png");
+	GLfloat vertices[] = {
+	// Positions // Colors // Texture Coords
+			0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
+			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
+			-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // Top Left
 			};
 	GLuint indices[] = {
 	// Note that we start from 0!
 			0, 1, 3, // First Triangle
 			1, 2, 3 // Second Triangle
-			};
-	GLfloat colours[] = { 1.0f, 0.0f, 0.0f, //
-			0.0f, 1.0f, 0.0f, //
-			0.0f, 0.0f, 1.0f, //0.5f, 0.5f, 0.5f
 			};
 	// Buffer ID
 	GLuint VBO, VAO, EBO, colours_vbo = 0;
@@ -81,21 +85,21 @@ int main() {
 	glBindVertexArray(VAO);
 	// 0. Copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 0.2 bind color buffer
-	glGenBuffers(1, &colours_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), NULL);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+			(GLvoid*) (3 * sizeof(GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+			(GLvoid*) (6 * sizeof(GLfloat)));
 	// 0.1. Copy our index array in a element buffer for OpenGL to use
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,	GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+	GL_STATIC_DRAW);
 
 	// 1. Then set the vertex attributes pointers
-
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
 	// unbind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	// 2. Unbind the VAO
@@ -111,9 +115,13 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Be sure to activate the shader
 		ourShader.Use();
+		// Bind Textures using texture units
+		glActiveTexture(GL_TEXTURE0);
+		texture1.use(ourShader.Program,0);
+		glActiveTexture(GL_TEXTURE1);
+		texture2.use(ourShader.Program,1);
 		glBindVertexArray(VAO);
-		// Now draw the triangle
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		glfwSwapBuffers(window);
 	}
--- shader/default.frag
+++ shader/default.frag
@@ -1,9 +1,10 @@
 #version 330 core
 in vec3 ourColor;
-
+in vec2 TexCoord;
 out vec4 color;
-
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
 void main()
 {
-    color = vec4(ourColor, 1.0f);
+	color = mix(texture(ourTexture1, TexCoord),texture(ourTexture2, vec2(TexCoord.x*2,1-TexCoord.y)),0.2);;
 }
