122c40c493e40cf4df9df3eac08efe8fd29b934c
shixinsb <shixinsb@sina.com>
2017-04-03 19:04:13 +0200

    lighting

--- main.cpp
+++ main.cpp
@@ -24,6 +24,9 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void do_movement();
 
 /* global parameter */
+
+// Light attributes
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 const GLuint WIDTH = 800, HEIGHT = 600;
 bool keys[1024];
 bool fall;
@@ -52,7 +55,7 @@ int main() {
 	// resizable?
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MyFirstRectangle",
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Lighting",
 			nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -74,13 +77,9 @@ int main() {
 	//glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, WIDTH, HEIGHT);
 
-	/* key function */
+	Shader lightingShader("shader/lighting.vs", "shader/lighting.frag");
+	Shader lampShader("shader/lamp.vs", "shader/lamp.frag");
 
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	Shader ourShader("shader/default.vs", "shader/default.frag");
 	/* texture */
 	glActiveTexture(GL_TEXTURE0);
 	Texture texture1("texture/container.jpg");
@@ -103,132 +102,168 @@ int main() {
 	glm::vec3(1.5f, 2.0f, -2.5f), //
 	glm::vec3(1.5f, 0.2f, -1.5f), //
 	glm::vec3(-1.3f, 1.0f, -1.5f) };
-	GLfloat vertices[] = { -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,	//
-			0.5f, -0.5f, -0.5f, 1.0f, 0.0f,	//
-			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,	//
-			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,	//
-			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,	//
-			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,	//
+	GLfloat vertices[] = { -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
+			0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
+			0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
+			0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
+			-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
+			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, //
 
-			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	//
-			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	//
-			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,	//
-			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,	//
-			-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,	//
-			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	//
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
+			0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
+			0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
+			0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
+			-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, //
 
-			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
-			-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,	//
-			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
-			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
-			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	//
-			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
+			-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, //
+			-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, //
+			-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, //
+			-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, //
+			-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, //
+			-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, //
 
-			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
-			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,	//
-			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
-			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
-			0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	//
-			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, //
+			0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, //
+			0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, //
+			0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, //
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, //
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, //
 
-			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
-			0.5f, -0.5f, -0.5f, 1.0f, 1.0f,	//
-			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	//
-			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	//
-			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	//
-			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,	//
+			-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, //
+			0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, //
+			0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, //
+			0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, //
+			-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, //
+			-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, //
 
-			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,	//
-			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,	//
-			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
-			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,	//
-			-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,	//
-			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f };
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, //
+			0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, //
+			0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, //
+			0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, //
+			-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, //
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f };
 
 	// Note that we start from 0!
 	GLfloat indices[] { 0, 1, 3, // First Triangle
 			1, 2, 3 // Second Triangle
 	};
-	// Buffer ID
-	GLuint VBO, VAO, EBO, colours_vbo = 0;
-
-	// generate one with a buffer ID
+	// First, set the container's VAO (and VBO)
+	GLuint VBO, containerVAO;
+	glGenVertexArrays(1, &containerVAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glGenVertexArrays(1, &VAO);
-	// -1 . Bind Vertex Array Object
-	glBindVertexArray(VAO);
-	// 0. Copy our vertices array in a buffer for OpenGL to use
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), NULL);
-//	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
-//			(GLvoid*) (3 * sizeof(GLfloat)));
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
-			(GLvoid*) (3 * sizeof(GLfloat)));
-	// 0.1. Copy our index array in a element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-	GL_STATIC_DRAW);
 
-	// 1. Then set the vertex attributes pointers
+	glBindVertexArray(containerVAO);
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+			(GLvoid*) 0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+			(GLvoid*) (3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(0);
-//	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-	// unbind VBO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// 2. Unbind the VAO
+	glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
+
+	// Then, we set the light's VAO (VBO stays the same. After all, the vertices are the same for the light object (also a 3D cube))
+	GLuint lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	// We only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need.
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Set the vertex attributes (only position data for the lamp))
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+			(GLvoid*) 0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+
+	/* key function */
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	// 3. Use our shader program when we want to render an object
 	glEnable(GL_DEPTH_TEST);
 	// Game loop
 	while (!glfwWindowShouldClose(window)) {
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 		do_movement();
 		// a state-setting function: set clear color
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Be sure to activate the shader
-		ourShader.Use();
-		// Bind Textures using texture units
-		glActiveTexture(GL_TEXTURE0);
-		texture1.use(ourShader.Program, 0);
-		glActiveTexture(GL_TEXTURE1);
-		texture2.use(ourShader.Program, 1);
-		/* GLM */
-		GLfloat currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		glBindVertexArray(VAO);
-		for (GLuint i = 0; i < 10; i++) {
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[i]);
-			GLfloat angle = 2.0f * i;
-			model = glm::rotate(model, (GLfloat) glfwGetTime() * angle,
-					glm::vec3(1.0f, 0.3f, 0.5f));
-			GLuint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-			glm::mat4 view;
-			view = camera.GetViewMatrix();
-			GLuint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-			glm::mat4 projection;
-			projection = glm::perspective(camera.Zoom,
-					(GLfloat) WIDTH / (GLfloat) HEIGHT, 0.1f, 1000.0f);
-			GLuint projectionLoc = glGetUniformLocation(ourShader.Program,
-					"projection");
-			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE,
-					glm::value_ptr(projection));
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		lightingShader.Use();
+		glm::mat4 tempRotate;
+		glm::vec4 tempLightPos = glm::rotate(tempRotate, (GLfloat) glfwGetTime() * 2.0f,
+				glm::vec3(0.0, 1.0, 0.0))*glm::vec4(lightPos,1.0f);
+		GLint objectColorLoc = glGetUniformLocation(lightingShader.Program,
+				"objectColor");
+		GLint lightColorLoc = glGetUniformLocation(lightingShader.Program,
+				"lightColor");
+		GLint lightPosLoc = glGetUniformLocation(lightingShader.Program,
+				"lightPos");
+		GLint viewPosLoc = glGetUniformLocation(lightingShader.Program,
+				"viewPos");
+		glUniform3f(viewPosLoc, camera.Position.x, camera.Position.y,
+				camera.Position.z);
+		glUniform3f(lightPosLoc, tempLightPos.x, tempLightPos.y, tempLightPos.z);
+		glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
+		glUniform3f(lightColorLoc, 1.0f, 0.5f, 1.0f);
+		// Create camera transformations
+		glm::mat4 view;
+		view = camera.GetViewMatrix();
+		glm::mat4 projection = glm::perspective(camera.Zoom,
+				(GLfloat) WIDTH / (GLfloat) HEIGHT, 0.1f, 100.0f);
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(lightingShader.Program,
+				"projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Draw the container (using container's vertex attributes)
+		glBindVertexArray(containerVAO);
+		glm::mat4 model;
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+
+		// Also draw the lamp object, again binding the appropriate shader
+		lampShader.Use();
+		// Get location objects for the matrices on the lamp shader (these could be different on a different shader)
+		modelLoc = glGetUniformLocation(lampShader.Program, "model");
+		viewLoc = glGetUniformLocation(lampShader.Program, "view");
+		projLoc = glGetUniformLocation(lampShader.Program, "projection");
+		// Set matrices
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		model = glm::mat4();
+		model = glm::rotate(model, (GLfloat) glfwGetTime() * 2.0f,
+				glm::vec3(0.0, 1.0, 0.0));
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		// Draw the light object (using light's vertex attributes)
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 		glfwSwapBuffers(window);
 	}
 	// delete vertex info
-	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteVertexArrays(1, &containerVAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 	/* terminate */
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
@@ -240,16 +275,16 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action,
 	if (key == GLFW_KEY_ESCAPE) {
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
-	if (action == GLFW_PRESS){
+	if (action == GLFW_PRESS) {
 		keys[key] = true;
-		if(key == GLFW_KEY_SPACE){
+		if (key == GLFW_KEY_SPACE) {
 			fall = false;
 		}
 	}
 
-	else if (action == GLFW_RELEASE){
+	else if (action == GLFW_RELEASE) {
 		keys[key] = false;
-		if(key == GLFW_KEY_ESCAPE){
+		if (key == GLFW_KEY_ESCAPE) {
 			fall == true;
 		}
 	}
@@ -268,7 +303,7 @@ void do_movement() {
 	if (keys[GLFW_KEY_SPACE])
 		camera.ProcessKeyboard(JUMP, deltaTime);
 	if (fall == true)
-		camera.ProcessKeyboard(FALL,deltaTime);
+		camera.ProcessKeyboard(FALL, deltaTime);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
